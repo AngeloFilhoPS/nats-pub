ae592299287d3d68521f7ea18a104c9bb7a554f7
angelo <ampsf@icomp.ufam.edu.br>
2021-07-05 10:10:58 -0400

    comentarios

--- src/main/kotlin/br/com/nats/angelo/core/service/InstrutorService.kt
+++ src/main/kotlin/br/com/nats/angelo/core/service/InstrutorService.kt
@@ -1,4 +1,42 @@
 package br.com.nats.angelo.core.service
 
-class InstrutorService {
+import br.com.nats.angelo.core.mapper.InstrutorConverter
+import br.com.nats.angelo.core.model.Instrutor
+import br.com.nats.angelo.core.port.InstrutorServicePort
+import br.com.nats.angelo.core.port.NatsServicePort
+import org.slf4j.LoggerFactory
+import javax.inject.Singleton
+
+@Singleton
+class InstrutorService(private val service: NatsServicePort):InstrutorServicePort {
+
+    private  val LOGGER = LoggerFactory.getLogger(this::class.java)
+
+    override fun sendCreateMessage(instrutor: Instrutor) {
+        service.sendCreateNats(
+            InstrutorConverter.instrutorRequestToInstrutorEvent(
+                InstrutorConverter.instrutorToInstrutorRequest(instrutor)
+            )
+        )
+    }
+
+    override fun sendDeleteMessage(instrutor: Instrutor) {
+
+        service.sendDeleteNats(
+            InstrutorConverter.instrutorRequestToInstrutorEvent(
+                InstrutorConverter.instrutorToInstrutorRequest(instrutor)
+            )
+        )
+
+    }
+
+    override fun sendUpdateMessage(instrutor: Instrutor) {
+
+        service.sendUpdateNats(
+            InstrutorConverter.instrutorRequestToInstrutorEvent(
+                InstrutorConverter.instrutorToInstrutorRequest(instrutor)
+            )
+        )
+
+    }
 }
